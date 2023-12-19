af3366a1dcea1b02ed1eb06d393b933ed3dd085e
VIRENDRA MAURYA <mauryaveer4050@gmail.com>
2023-12-19 10:56:47 +0530

    This is my first commit

---
+++ src/pages/room/Room.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+
+export default function Room() {
+  const { roomId } = useParams();
+
+  // myMeeting Function
+  const myMeeting = async (element) => {
+    const appID = 765598355;
+    const serverSecret = "9b6b2212ab008afc8f9546c27c0e2936";
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomId,
+      Date.now().toString(),
+      "Enter your Name"
+    );
+
+    const zc = ZegoUIKitPrebuilt.create(kitToken);
+    zc.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: "Copy Link",
+          url: `https://videocallweb.vercel.app/room/${roomId}`,
+        },
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.OneONoneCall,
+      },
+      showScreenSharingButton: true,
+    });
+  };
+  return (
+    <div>
+      <div ref={myMeeting} />
+    </div>
+  );
+}
